aaffabb2df792ad95de7976b78956d71bc53c8ef
Simon Schurter <simon.schurter@customweb.com>
2020-11-25 16:39:15 +0100

    Release 1.1.0

---
+++ custom/plugins/TrustPaymentsPayment/vendor/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Trust Payments Shopware 5
+ *
+ * This Shopware 5 extension enables to process payments with Trust Payments (https://www.trustpayments.com//).
+ *
+ * @package TrustPayments_Payment
+ * @author wallee AG (http://www.wallee.com/)
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache Software License (ASL 2.0)
+ */
+
+require_once __DIR__ . '/TrustPayments/Sdk/autoload.php';
